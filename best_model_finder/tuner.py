24c50190f99b8ee9340b3758bf24851dc41b1bc4
kiranaradhya20 <80210004+kiranaradhya20@users.noreply.github.com>
2023-07-05 19:11:14 +0530

    Update tuner.py

--- best_model_finder/tuner.py
+++ best_model_finder/tuner.py
@@ -6,9 +6,7 @@ from sklearn.metrics  import roc_auc_score,accuracy_score
 class Model_Finder:
     """
                 This class shall  be used to find the model with best accuracy and AUC score.
-                Written By: iNeuron Intelligence
-                Version: 1.0
-                Revisions: None
+            
 
                 """
 
@@ -26,9 +24,6 @@ class Model_Finder:
                                 Output: The model with the best parameters
                                 On Failure: Raise Exception
 
-                                Written By: iNeuron Intelligence
-                                Version: 1.0
-                                Revisions: None
 
                         """
         self.logger_object.log(self.file_object, 'Entered the get_best_params_for_random_forest method of the Model_Finder class')
@@ -73,10 +68,6 @@ class Model_Finder:
                                                      Use Hyper Parameter Tuning.
                                         Output: The model with the best parameters
                                         On Failure: Raise Exception
-
-                                        Written By: iNeuron Intelligence
-                                        Version: 1.0
-                                        Revisions: None
 
                                 """
         self.logger_object.log(self.file_object,
@@ -124,9 +115,6 @@ class Model_Finder:
                                                 Output: The best model name and the model object
                                                 On Failure: Raise Exception
 
-                                                Written By: iNeuron Intelligence
-                                                Version: 1.0
-                                                Revisions: None
 
                                         """
         self.logger_object.log(self.file_object,
